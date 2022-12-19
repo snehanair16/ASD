31c294b8ffb9afcb396d5c72f67a97d45274a44a
snehanair16 <cssneha001@gmail.com>
2022-12-19 16:57:54 +0530

    3 PRO

--- 3.sh
+++ 3.sh
@@ -3,6 +3,9 @@ echo "please enter num1"
 read num1
 echo "please  enter  num2"
 read num2
-if [$num1 -eq $num2}
+if [ $num1 -eq $num2 ]
 then
-echo "
+echo "hey,the num $num1 & $num2 are equal"
+else
+echo " the num $num1 & $num2 are not equal"
+fi
